4b12e9d52041a4208ed0154e6dd2107566334615
IvyLeVay <00265381@student.necc.edu>
2014-05-16 13:58:16 -0400

    Initial Commit

---
+++ MWOBS/MWAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MWAppDelegate.h
+//  MWOBS
+//
+//  Created by Rachel Lareau on 5/16/14.
+//  Copyright (c) 2014 Dyn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MWAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MWOBS/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MWOBS
+//
+//  Created by Rachel Lareau on 5/16/14.
+//  Copyright (c) 2014 Dyn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MWAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MWAppDelegate class]));
+    }
+}
